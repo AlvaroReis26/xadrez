91a8aaf842529da2339a19f95ddfbb51377af1a6
Alvaro Pereira <alvaropereirareis@yahoo.com.br>
2019-07-12 21:47:44 -0300

    Alterado teste1

--- aula147/Program.cs
+++ aula147/Program.cs
@@ -7,6 +7,8 @@ namespace aula147
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Teste commit");
         }
     }
 }
